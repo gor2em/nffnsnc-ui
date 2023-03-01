fc94bd208ab473e81656a92a1d8f4212aef7370c
mekrograca <grkem.squash99@gmail.com>
2023-03-01 16:41:46 +0300

    Heading3 component

--- src/components/Heading3.vue
+++ src/components/Heading3.vue
@@ -3,10 +3,14 @@ defineProps({
     title: {
         type: String,
         required: true
+    },
+    hasMargin: {
+        type: Boolean,
+        default: true
     }
 })
 </script>
 <template>
-    <h3 class="font-bold text-lg mb-2">{{ title }}</h3>
+    <h3 class="font-bold text-lg" :class="hasMargin ? 'mb-2' : ''">{{ title }}</h3>
 </template>
 <style scoped></style>
