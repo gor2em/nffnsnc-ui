95faed2e0835b249b6f13533eb1525d324336f5a
mekrograca <grkem.squash99@gmail.com>
2023-03-01 16:41:33 +0300

    followersData

---
+++ src/data/followers.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    username: "glory",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+  },
+  {
+    username: "roce",
+    profile_image:
+      "https://i.pinimg.com/originals/37/48/ec/3748eca2899fa91e18c9f32121234e6a.jpg",
+  },
+  {
+    username: "mongo",
+    profile_image:
+      "https://i.pinimg.com/736x/9f/e2/50/9fe250816bbdc9af220a0cdb471b340d.jpg",
+  },
+  {
+    username: "sahe",
+    profile_image: "",
+  },
+  {
+    username: "neo",
+    profile_image:
+      "https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:eco%2Cw_1200/MTc0NTA2NDM0MTA3NTQ5Njg2/why-neo-is-the-one-in-the-matrix-trilogy.png",
+  },
+];
