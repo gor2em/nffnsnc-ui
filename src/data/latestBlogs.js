be1b775999e210a0f3b21b9de012fd267258b5e0
mekrograca <grkem.squash99@gmail.com>
2023-03-01 16:35:50 +0300

    latestBlogs data

---
+++ src/data/latestBlogs.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    img: "https://i.pinimg.com/564x/8b/2a/ac/8b2aac9176b240f78da675cb07984b43.jpg",
+    title: "Hayalperest Kedi",
+    category: "Felsefe",
+    user: {
+      name: "whoben",
+      profile_image:
+        "https://i.pinimg.com/736x/6a/ce/af/6aceaf789da1047cf4d46bb9c2e15569.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/eb/99/7d/eb997d11bd0254c144f93bac4bfd3089.jpg",
+    title: "Matrix Nasıl var oldu?",
+    category: "Sinema",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  // {
+  //   img: "https://64.media.tumblr.com/041007455cc40fa55b852260868a743e/tumblr_mjyo9nKMrX1qff6llo1_500.gif",
+  //   title: "Jon Snow adam mıdır?",
+  //   category: "Sinema",
+  //   user: {
+  //     name: "glory",
+  //     profile_image:
+  //       "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+  //   },
+  // },
+  {
+    img: "https://www.rollingstone.com/wp-content/uploads/2022/04/TCDBRBA_EC027.jpg?w=1581&h=1054&crop=1",
+    title: "Breaking Bad Felsefesi",
+    category: "Sinema",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/00/63/1b/00631b5c98634d6eb41f1b07cababcaf.jpg",
+    title: "Sessiz Çığlık",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c8/a5/06/c8a50606d14dc437c50702754b625478.jpg",
+    title: "Düşüncesel Çöküşler",
+    category: "Hayat",
+    user: {
+      name: "astra",
+      profile_image:
+        "https://i.pinimg.com/564x/b2/63/fd/b263fd80051be01f51c754c0fb2ee81d.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c1/1c/46/c11c46af3e59f2c3261caa8e69d9b082.jpg",
+    title: "Bu bir aşk hikayesi değil!",
+    category: "Sinema",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/b7/71/53/b771538e72685d31085d39d1335dd8ef.jpg",
+    title: "İmkansız Aşk",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/00/63/1b/00631b5c98634d6eb41f1b07cababcaf.jpg",
+    title: "Sessiz Çığlık",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c8/a5/06/c8a50606d14dc437c50702754b625478.jpg",
+    title: "Düşüncesel Çöküşler",
+    category: "Hayat",
+    user: {
+      name: "astra",
+      profile_image:
+        "https://i.pinimg.com/564x/b2/63/fd/b263fd80051be01f51c754c0fb2ee81d.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c1/1c/46/c11c46af3e59f2c3261caa8e69d9b082.jpg",
+    title: "Bu bir aşk hikayesi değil!",
+    category: "Sinema",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/b7/71/53/b771538e72685d31085d39d1335dd8ef.jpg",
+    title: "İmkansız Aşk",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/00/63/1b/00631b5c98634d6eb41f1b07cababcaf.jpg",
+    title: "Sessiz Çığlık",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c8/a5/06/c8a50606d14dc437c50702754b625478.jpg",
+    title: "Düşüncesel Çöküşler",
+    category: "Hayat",
+    user: {
+      name: "astra",
+      profile_image:
+        "https://i.pinimg.com/564x/b2/63/fd/b263fd80051be01f51c754c0fb2ee81d.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/c1/1c/46/c11c46af3e59f2c3261caa8e69d9b082.jpg",
+    title: "Bu bir aşk hikayesi değil!",
+    category: "Sinema",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+  {
+    img: "https://i.pinimg.com/564x/b7/71/53/b771538e72685d31085d39d1335dd8ef.jpg",
+    title: "İmkansız Aşk",
+    category: "Hayat",
+    user: {
+      name: "glory",
+      profile_image:
+        "https://pbs.twimg.com/profile_images/1629180967842725892/o-JlwSj9_400x400.jpg",
+    },
+  },
+];
