2eb73d8031dbd0720b456a723cc208b48c8103ae
mekrograca <grkem.squash99@gmail.com>
2023-03-05 01:09:21 +0300

    light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,18 +4,19 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
-      screens: {
-        xs: "614px",
-        sm: "1002px",
-        md: "1022px",
-        lg: "1092px",
-        xl: "1180px",
-        "2xl": "1280px",
-      },
+      // screens: {
+      //   xs: "614px",
+      //   sm: "1002px",
+      //   md: "1022px",
+      //   lg: "1092px",
+      //   xl: "1180px",
+      //   "2xl": "1280px",
+      // },
 
       colors: {
         theme: {
           primaryBg: "var(--primary-bg-color)",
+          primaryNav: "var(--primary-nav-color)",
           primaryHead: "var(--primary-head-color)",
           primaryText: "var(--primary-text-color)",
           primaryTextDark: "var(--primary-text-dark-color)",
@@ -26,6 +27,7 @@ module.exports = {
           primaryIconActive: "var(--primary-icon-active-color)",
 
           secondaryBg: "var(--secondary-bg-color)",
+          backdropMain: "var(--backdrop-main-bg-color)",
 
           fromGradient: "var(--from-gradient)",
           toGradient: "var(--to-gradient)",
