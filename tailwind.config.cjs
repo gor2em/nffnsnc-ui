4268de3e85b2fb9af77f3d3ecbd56968d17cfa15
mekrograca <grkem.squash99@gmail.com>
2023-03-04 23:08:26 +0300

    created root colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
+      screens: {
+        xs: "614px",
+        sm: "1002px",
+        md: "1022px",
+        lg: "1092px",
+        xl: "1180px",
+        "2xl": "1280px",
+      },
+
       colors: {
         theme: {
-          primaryBackground: "var(--primary-bg-color)",
-          primary: "var(--primary-color)",
-          secondaryBackground: "var(--secondary-bg-color)",
-          secondary: "var(--secondary-color)",
+          primaryBg: "var(--primary-bg-color)",
+          primaryHead: "var(--primary-head-color)",
+          primaryText: "var(--primary-text-color)",
+          primaryTextDark: "var(--primary-text-dark-color)",
+          primaryMoreTextDark: "var(--primary-text-more-dark-color)",
+          primaryTextSuccess: "var(--primary-text-success-color)",
+          primaryBorder: "var(--primary-border-color)",
+          primaryIcon: "var(--primary-icon-color)",
+          primaryIconActive: "var(--primary-icon-active-color)",
+
+          secondaryBg: "var(--secondary-bg-color)",
+
+          fromGradient: "var(--from-gradient)",
+          toGradient: "var(--to-gradient)",
+          viaGradient: "var(--via-gradient)",
         },
       },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
+
       fontFamily: {
         montserrat: ["Montserrat"],
       },
